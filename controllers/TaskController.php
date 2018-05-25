930881990b3be922b25283252e43bec0e2967564
SERGEI OGARKOV <pdpdga@mail.ru>
2018-05-25 22:35:34 +0300

    Homework 1 done

---
+++ controllers/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergey
+ * Date: 25.05.2018
+ * Time: 21:29
+ */
+
+namespace app\controllers;
+
+
+use app\models\Test;
+use yii\web\Controller;
+
+class TaskController extends Controller
+{
+    public function actionIndex(){
+
+        $model = new Test();
+
+        $model->load([
+            'params'  => [],
+            'Test' => ['title' => 'Основная страница таск-трекера','content' => 'Yii2']
+        ]);
+
+        //$model -> attributes = ['title' => 'Основная страница таск-трекера','content' => 'Yii2'];
+
+        $model->validate();
+        $model->getErrors();
+
+        return $this->render('index', ['title' => $model->title, 'content' => $model->content]);
+    }
+}
